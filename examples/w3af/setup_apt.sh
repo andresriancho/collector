dcacfcb42431deec5262fff50960ab42d111c528
Andres Riancho <andres.riancho@gmail.com>
2014-09-01 18:21:54 -0300

    Trying to fix issue with apt

--- examples/w3af/setup_apt.sh
+++ examples/w3af/setup_apt.sh
@@ -5,5 +5,7 @@ set -e
 
 echo 'APT::Get::Assume-Yes "true";' > /etc/apt/apt.conf.d/90forceyes
 echo 'APT::Get::force-yes "true";' >> /etc/apt/apt.conf.d/90forceyes
+echo 'Acquire::http::Pipeline-Depth "0";' >> /etc/apt/apt.conf.d/90-fix-s3
 
+sed -i 's/us-east-1/us/g;' /etc/apt/sources.list
 apt-get update --quiet > /dev/null
