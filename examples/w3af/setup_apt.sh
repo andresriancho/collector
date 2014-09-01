ffc7fc81d13166db1dd25b0250bd3ffde1893fe3
Andres Riancho <andres.riancho@gmail.com>
2014-09-01 18:52:16 -0300

    Remove apt cache

--- examples/w3af/setup_apt.sh
+++ examples/w3af/setup_apt.sh
@@ -8,4 +8,5 @@ echo 'APT::Get::force-yes "true";' >> /etc/apt/apt.conf.d/90forceyes
 echo 'Acquire::http::Pipeline-Depth "0";' >> /etc/apt/apt.conf.d/90-fix-s3
 
 sed -i 's/us-east-1/us/g;' /etc/apt/sources.list
+rm /var/cache/apt/* -rf
 apt-get update --quiet > /dev/null
